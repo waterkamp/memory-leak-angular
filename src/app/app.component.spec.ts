9dfe360ca1747a1376e7828aac6416ff74a400b7
Oliver Waterkamp <oliver.waterkamp@2BIT.ch>
2023-03-09 21:19:00 +0100

    renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'littleTest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('littleTest');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('littleTest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('memoryLeak app is running!');
   });
 });
